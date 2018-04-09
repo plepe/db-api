a3028f7a087a3efc76407df652eafbef10662526
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-04-09 13:21:12 +0200

    Example/ModulekitForm: save changes

--- src/DBApiViewModulekitForm.js
+++ src/DBApiViewModulekitForm.js
@@ -9,6 +9,9 @@ class DBApiViewModulekitForm extends DBApiView {
         })
       }
 
+      let domForm = document.createElement('form')
+      dom.appendChild(domForm)
+
       let options = {
         type: 'array',
         default: 1
@@ -16,8 +19,34 @@ class DBApiViewModulekitForm extends DBApiView {
       let formDef = { def: this.def, type: 'form' }
 
       this.form = new form(null, formDef, options)
-      this.form.show(dom)
+      this.form.show(domForm)
       this.form.set_data(result)
+
+      let input = document.createElement('input')
+      input.type = 'submit'
+      input.value = lang('save')
+      domForm.appendChild(input)
+
+      domForm.onsubmit = () => {
+        let data = this.form.get_data()
+        let changeset = []
+
+        changeset.push({
+          action: 'insert-update',
+          table: this.query.table,
+          data: data
+        })
+
+        this.api.do(changeset, (err, result) => {
+          this.emit('save', {
+            form: this.form,
+            error: err,
+            result: result
+          })
+        })
+
+        return false
+      }
 
       this.emit('show', {
         form: this.form,
