d45553b839c7204649d262aaed6cbf47ee629749
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-04-19 11:41:02 +0200

    DBApiViewTwig: test (show with step=0)

--- test/test.js
+++ test/test.js
@@ -175,4 +175,31 @@ describe('DBApiViewTwig', () => {
     })
     view.show(dom, { step: 2 })
   })
+
+  it('show all (step=0)', (done) => {
+    let doneCount = 0
+    function stepDone (err) {
+      if (++doneCount >= 3) {
+        done()
+      }
+    }
+
+    let view = api.createView('Twig', '{{ entry.id }}: {{ entry.commentsCount }}\n', { twig })
+    let dom = document.createElement('div')
+    view.set_query({ table: 'test2' })
+    view.once('loadstart', ev => {
+      stepDone()
+    })
+    view.once('loadend', ev => {
+      stepDone()
+    })
+    view.once('show', ev => {
+      assert.equal(ev.error, null, 'Error should be null')
+      assert.deepEqual(ev.result, [ '1: 2\n', '2: 1\n', '3: 2\n', '4: 2\n' ])
+      let expected = '<div>1: 2\n</div><div>2: 1\n</div><div>3: 2\n</div><div>4: 2\n</div>'
+      assert.equal(dom.innerHTML, expected)
+      stepDone()
+    })
+    view.show(dom, { step: 0 })
+  })
 })
