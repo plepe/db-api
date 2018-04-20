9681a9a30684a8ee62eb2c6a73c7e145c41befe5
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-04-20 10:37:34 +0200

    DBApiExtLeaflet: allow several maps per template

--- src/DBApiExtLeaflet.js
+++ src/DBApiExtLeaflet.js
@@ -6,40 +6,41 @@ class DBApiExtLeaflet extends DBApiExt {
 
     view.on('showEntry', ev => {
       let divs = ev.dom.getElementsByTagName('map')
-      if (divs.length === 0) {
-        return
-      }
 
-      let layers
-      if ('layers' in options) {
-        layers = options.layers
-      } else {
-        layers = {
-          'OpenStreetMap Mapnik': {
-              url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-              attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }
+      for (var i = 0; i < divs.length; i++) {
+        let div = divs[i]
+
+        let layers
+        if ('layers' in options) {
+          layers = options.layers
+        } else {
+          layers = {
+            'OpenStreetMap Mapnik': {
+                url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              }
+          }
         }
+
+        let mapLayers = {}
+        for (var k in layers) {
+          mapLayers[k] = L.tileLayer(layers[k].url, layers[k])
+        }
+
+        var defaultLayer = mapLayers[Object.keys(mapLayers)[0]]
+
+        var map = L.map(div, {
+          center: [ ev.entry[this.options.latitudeField], ev.entry[this.options.longitudeField] ],
+          zoom: this.options.zoom || 17,
+          layers: defaultLayer
+        })
+
+        if (Object.keys(mapLayers).length > 1) {
+          L.control.layers(mapLayers).addTo(map)
+        }
+
+        L.marker([ ev.entry[this.options.latitudeField], ev.entry[this.options.longitudeField] ]).addTo(map)
       }
-
-      let mapLayers = {}
-      for (var k in layers) {
-        mapLayers[k] = L.tileLayer(layers[k].url, layers[k])
-      }
-
-      var defaultLayer = mapLayers[Object.keys(mapLayers)[0]]
-
-      var map = L.map(divs[0], {
-        center: [ ev.entry[this.options.latitudeField], ev.entry[this.options.longitudeField] ],
-        zoom: this.options.zoom || 17,
-        layers: defaultLayer
-      })
-
-      if (Object.keys(mapLayers).length > 1) {
-        L.control.layers(mapLayers).addTo(map)
-      }
-
-      L.marker([ ev.entry[this.options.latitudeField], ev.entry[this.options.longitudeField] ]).addTo(map)
     })
   }
 }
