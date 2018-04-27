6870d3803e8e134af61b936ec3337d4da91c494c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-04-27 22:08:38 +0200

    DBApiExtLeaflet: use leafletMap as common module

--- src/DBApiExtLeaflet.js
+++ src/DBApiExtLeaflet.js
@@ -1,4 +1,5 @@
 const DBApiExt = require('./DBApiExt')
+const leafletMap = require('./leafletMap')
 
 class DBApiExtLeaflet extends DBApiExt {
   constructor (view, options={}) {
@@ -17,43 +18,8 @@ class DBApiExtLeaflet extends DBApiExt {
             mapOptions[k] = o[k]
           }
         }
-        console.log(mapOptions)
 
-        let layers
-        if ('layers' in mapOptions) {
-          layers = mapOptions.layers
-        } else {
-          layers = {
-            'OpenStreetMap Mapnik': {
-                url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              }
-          }
-        }
-
-        let mapLayers = {}
-        for (var k in layers) {
-          mapLayers[k] = L.tileLayer(layers[k].url, layers[k])
-        }
-
-        let defaultLayer
-        if (mapOptions.layer) {
-          defaultLayer = mapLayers[mapOptions.layer]
-        } else {
-          defaultLayer = mapLayers[Object.keys(mapLayers)[0]]
-        }
-
-        var map = L.map(div, {
-          center: [ ev.entry[mapOptions.latitudeField], ev.entry[mapOptions.longitudeField] ],
-          zoom: mapOptions.zoom || 17,
-          layers: defaultLayer
-        })
-
-        if (Object.keys(mapLayers).length > 1) {
-          L.control.layers(mapLayers).addTo(map)
-        }
-
-        L.marker([ ev.entry[mapOptions.latitudeField], ev.entry[mapOptions.longitudeField] ]).addTo(map)
+        leafletMap(div, mapOptions, ev.entry)
       }
     })
   }
